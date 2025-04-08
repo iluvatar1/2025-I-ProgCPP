59c9649230f619a7e8b892c1c6a1c5a0953a5ac7
iluvatar1 <woquendo@gmail.com>
2025-04-08 11:08:35 -0500

    Programa que muestra como saludar en la salida estandar

---
+++ 2025-04-04-IntroCPP/hola.cpp
@@ -0,0 +1,13 @@
+// This prints a comment
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+    std::string name; 
+    std::cout << "Hello, whats your name?\n";
+    //std::cin >> name; // lee hbasta el primer espacio
+    std::getline(std::cin, name);
+    std::cout << "Hello, " + name + "\n" ;
+    return 0;
+}
