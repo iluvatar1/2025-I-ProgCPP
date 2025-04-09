6b7aba0bbb8bec159e53cc978a134652d16918d1
iluvatar1 <woquendo@gmail.com>
2025-04-09 08:32:20 -0500

    Se incluye repeticion

--- 2025-04-09-IntroCPP/guess_game.cpp
+++ 2025-04-09-IntroCPP/guess_game.cpp
@@ -13,15 +13,19 @@ int main(void) {
 
     const int NUM = 10;
     int guessed_number;
-    std::cout << "Adivina un numero entre 1 y 100:\n";
-    std::cin >> guessed_number;
-    
-    if (guessed_number == NUM) {
-        std::cout << "GANASTE!!!!\n";
-    } else if (guessed_number > NUM){
-        std::cout << "Te pasaste.\n";
-    } else { // es menor
-        std::cout << "Te falta.\n";
+
+    for( ; ; ) {
+        std::cout << "Adivina un numero entre 1 y 100:\n";
+        std::cin >> guessed_number;
+        
+        if (guessed_number == NUM) {
+            std::cout << "GANASTE!!!!\n";
+            break;
+        } else if (guessed_number > NUM){
+            std::cout << "Te pasaste.\n";
+        } else { // es menor
+            std::cout << "Te falta.\n";
+        }
     }
 
     return 0;
