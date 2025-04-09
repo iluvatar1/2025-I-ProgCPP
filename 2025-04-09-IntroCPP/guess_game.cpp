64d58b9e21f6024cde45161a9e70c2613202ab43
iluvatar1 <woquendo@gmail.com>
2025-04-09 07:49:49 -0500

    Se anhade condicion logica para verificar si se adivino el numero

--- 2025-04-09-IntroCPP/guess_game.cpp
+++ 2025-04-09-IntroCPP/guess_game.cpp
@@ -10,10 +10,17 @@ Informar si lo adivino o no
 #include <iostream>
 
 int main(void) {
+
     const int NUM = 10;
     int guessed_number;
     std::cout << "Adivina un numero entre 1 y 100:\n";
     std::cin >> guessed_number;
     
+    if (guessed_number == NUM) {
+        std::cout << "GANASTE!!!!\n";
+    } else {
+        std::cout << "No ganaste. Intentalo de nuevo\n";
+    }
+
     return 0;
 }
