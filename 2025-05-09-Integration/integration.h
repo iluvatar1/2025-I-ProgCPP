6a94110eefd573f5fdb8169abdc4560672335ede
iluvatar1 <woquendo@gmail.com>
2025-05-09 07:56:24 -0500

    Added inityial integration files

---
+++ 2025-05-09-Integration/integration.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <functional>
+
+// using fptr = double(double);
+using fptr = std::function<double(double)>;
+
+// trapezoid(0.0, 1.0, 10, fun)
+double trapezoid(double a, double b, int N, fptr fun);
---
+++ 2025-05-09-Integration/main_integration.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "integration.h"
+
+double f(double x);
+
+int main(void)
+{
+    std::cout << trapezoid(0.0, 1.0, 10, f) << "\n";
+    std::cout << trapezoid(0.0, 1.0, 1000, f) << "\n";
+    std::cout << trapezoid(0.0, 1.0, 10000000, f) << "\n";
+    return 0;
+}
+
+double f(double x)
+{
+    return x*x;
+}
