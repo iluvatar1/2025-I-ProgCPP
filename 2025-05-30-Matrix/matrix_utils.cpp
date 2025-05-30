91af43a2d591f15c0c7396747a863e29db264119
iluvatar1 <woquendo@gmail.com>
2025-05-30 08:34:01 -0500

    Base matrix utils

---
+++ 2025-05-30-Matrix/matrix_utils.cpp
@@ -0,0 +1,32 @@
+#include "matrix_utils.h"
+
+void fill_matrix(std::vector<double> & data, int m, int n)
+{
+  for (int ii = 0; ii < m; ++ii) {
+    for (int jj = 0; jj < n; ++jj) {
+      data[ii*n + jj] = ii*n+jj; // A_(i, j) = i*n + j = id
+    }
+  }
+}
+
+void print_matrix(const std::vector<double> & data, int m, int n)
+{
+  for (int ii = 0; ii < m; ++ii) {
+    for (int jj = 0; jj < n; ++jj) {
+      std::cout << data[ii*n + jj] << " ";
+    }
+    std::cout << "\n";
+  }
+  std::cout << "\n";
+}
+
+void transpose_matrix(const std::vector<double> & datain, int m, int n,
+                      std::vector<double> & dataout)
+{
+  // TODO
+   for (int ii = 0; ii < m; ++ii) {
+     for (int jj = 0; jj < n; ++jj) {
+       dataout[jj*m + ii] = datain[ii*n + jj];
+     }
+   }
+}
