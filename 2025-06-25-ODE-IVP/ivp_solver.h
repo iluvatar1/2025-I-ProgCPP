fac6835c31c9b70b88c1e1a3a045d7e4bf73cf99
iluvatar1 <woquendo@gmail.com>
2025-06-25 08:26:24 -0500

    Added soorver ivp with euler

---
+++ 2025-06-25-ODE-IVP/ivp_solver.h
@@ -0,0 +1,28 @@
+
+#pragma once
+
+// ivp_solver.h
+#include <iostream>
+#include <valarray>
+#include <functional>
+
+
+// function template to work with "any" type
+template <class deriv_t, class system_t, class printer_t>
+void integrate_euler(deriv_t fderiv, system_t & s, double tinit, double tend, double dt, printer_t writer)
+{
+    // vector to store derivs
+    system_t dsdt(s.size());
+
+    // time loop
+    for(double t = tinit; t <= tend; t = t + dt) { // NOTE: Last time step not necessarily tf
+        // compute derivs
+        fderiv(s, dsdt, t);
+
+        // compute new state. NOTE: Not using components, assuming valarray or similar
+        s = s + dt*dsdt; // Euler
+
+        // write new state
+        writer(s, t);
+      }
+}
